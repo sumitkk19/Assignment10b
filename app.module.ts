b25b6a96ff15dfc3c7e30e9719789f57dce32e8a
sumitkk19 <65333951+sumitkk19@users.noreply.github.com>
2023-04-22 18:31:22 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { BooksComponent } from './books/books.component';
+import { AppRoutingModule } from './app.routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TechnologiesComponent,
+    BooksComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
